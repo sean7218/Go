0fa42ec43bd15e93cf87b901b19850f59074f361
Xinyu Zhang <lhzhxy_87@hotmail.com>
2017-07-27 00:17:22 -0400

    second Commit

--- hello.go
+++ hello.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+
+import (
+  "fmt"
+  "stringutil"
+  "time"
+  )
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+  fmt.Printf(stringutil.Reverse("!oG, olleH"))
+  fmt.Printf("\nThe time is ", time.Now())
+  a, b := swap("Swap", "ME")
+  fmt.Printf(a, b)
+}
+
+func swap(x, y string) (string, string) {
+  return y, x
 }
